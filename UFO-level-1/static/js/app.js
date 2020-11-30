b7448aa8fed8439766e18adc769c172f9d3fb977
brettonmaybee <brettonmaybee@users.noreply.github.com>
2020-11-30 02:09:39 -0800

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody =d3.select("tbody");
+
+console.log(tableData);
+
+tableData.forEach(function(ufoReport){
+    console.log(ufoReport)
+});
+
+tableData.forEach(function(ufoReport){
+   var row =tbody.append("tr");
+});
+
+//tableData.forEach(function(ufoReport){
+  //  var row =tbody.forEach("tr");
+    //Object.defineProperties(ufoReport).forEach(function([key,value]){
+    //    console.log(key,value);
+    //})
+//})
