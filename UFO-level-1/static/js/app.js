034b00b06cbebc1fcdaf2bb4ef3494e7f9ddd90e
brettonmaybee <brettonmaybee@users.noreply.github.com>
2021-01-12 16:27:41 -0800

    commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,16 +1,40 @@
 // from data.js
 var tableData = data;
-
 var tbody =d3.select("tbody");
+//console.log(tableData);
+var button = d3.select("#filter-btn");
+var inputField = d3.select("#datetime");
 
-console.log(tableData);
+var searchDate=
+inputField.on("change", function() {
+  var filterDate = d3.event.target.value;
+  //button.on("click", function() {  
+  //});
+  console.log(filterDate);
+});
+
+
 
 tableData.forEach((ufoReport)=>{
-  console.log(ufoReport)
+  //console.log(ufoReport)
   var row =tbody.append("tr");
   Object.entries(ufoReport).forEach(([key,value])=>{
-    console.log(key,value);
+    //console.log(key,value);
     var cell= row.append("td");
     cell.text(value);
   });
-});
+})
+
+// var button = d3.select("#filter-btn");
+// var inputField = d3.select("#input-field");
+
+// inputField.on("change", function() {
+//   var newText = d3.event.target.value;
+//   console.log(newText);
+// });
+
+// button.on("click", function() {
+//   console.log(inputField);
+//   //console.log(d3.event.target);
+// })});
+
